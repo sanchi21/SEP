e69f180448ef4146f258d7b66102c009ec95b9be
HyperJaguar <sabhayans@gmail.com>
2015-03-22 15:02:12 +0530

    2015.03.22 15.01 Stable Version 1.1

---
+++ app/Http/Middleware/role.php
@@ -0,0 +1,29 @@
+<?php namespace App\Http\Middleware;
+
+use Closure;
+use Auth;
+
+class role {
+
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Closure  $next
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next)
+	{
+
+        if (Auth::user() && Auth::user()->permissions == "Administrator Full" )
+        {
+            return $next($request);
+        }
+
+        return redirect('accessDenied');
+
+
+//		return $next($request);
+	}
+
+}
---
+++ resources/views/home-adminLimit.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abhay adm
+ * Date: 3/22/2015
+ * Time: 1:20 PM
+ */ ?>
+Admin Limited
